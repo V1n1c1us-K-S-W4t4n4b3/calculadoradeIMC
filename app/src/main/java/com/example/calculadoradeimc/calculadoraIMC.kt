bd1490bd5358d4adc5c1aeee2fd661e55e6e7c2d
V1n1c1us-KS-W4t4n4b3 <vinikswatanabe@outlook.com>
2023-08-31 22:32:01 -0300

    segunda tela

--- app/src/main/java/com/example/calculadoradeimc/calculadoraIMC.kt
+++ app/src/main/java/com/example/calculadoradeimc/calculadoraIMC.kt
@@ -3,9 +3,11 @@ package com.example.calculadoradeimc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity2 : AppCompatActivity() {
+class calculadoraIMC : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_calculadora_imc)
     }
 }
