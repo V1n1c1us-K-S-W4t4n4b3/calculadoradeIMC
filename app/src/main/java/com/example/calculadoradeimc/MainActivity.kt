dc3c02fa590467dc8f1a42f4177fedbaef9bda07
V1n1c1us-KS-W4t4n4b3 <vinikswatanabe@outlook.com>
2023-09-02 02:01:24 -0300

    calculadora funcional

--- app/src/main/java/com/example/calculadoradeimc/MainActivity.kt
+++ app/src/main/java/com/example/calculadoradeimc/MainActivity.kt
@@ -7,14 +7,14 @@ import com.example.calculadoradeimc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.button.setOnClickListener {
-            val NavegarSegundaTela = Intent(this,calculadoraIMC::class.java)
+            val NavegarSegundaTela = Intent(this, calculadoraIMC::class.java)
             startActivity(NavegarSegundaTela)
 
         }
--- app/src/main/java/com/example/calculadoradeimc/calculadoraIMC.kt
+++ app/src/main/java/com/example/calculadoradeimc/calculadoraIMC.kt
@@ -3,25 +3,46 @@ package com.example.calculadoradeimc
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.example.calculadoradeimc.databinding.ActivityCalculadoraImcBinding
 
 class calculadoraIMC : AppCompatActivity() {
     private lateinit var binding: ActivityCalculadoraImcBinding
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityCalculadoraImcBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_calculadora_imc)
 
-     binding.buttonBackIMC.setOnClickListener {
-         val VoltarTelaPrincipal = Intent(this,MainActivity::class.java)
-         startActivity(VoltarTelaPrincipal)
-     }   
+
+        val peso = findViewById(R.id.editPeso) as EditText
+        val altura = findViewById(R.id.editAltura) as EditText
+        val total = findViewById(R.id.textResultado) as TextView
+        val calcular = findViewById(R.id.buttonCalcular) as Button
+        val voltar = findViewById(R.id.button_back_IMC) as Button
+
+
+        calcular.setOnClickListener {
+            val peso = peso.text.toString().toDouble();
+            val altura = altura.text.toString().toDouble();
+
+            val result = peso / (altura * altura)
+
+            total.setText(result.toString())
+
+        }
+        voltar.setOnClickListener {
+            val VoltarTelaPrincipal = Intent(this, MainActivity::class.java)
+                 startActivity(VoltarTelaPrincipal)
+        }
+
+
+        
+        }
 
     }
 
 
-
-
-
-}
+//}
